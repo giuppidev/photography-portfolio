4e75036ced0cf582947cc5f014402b71e0168f6c
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-12 18:20:09 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Roberto De Martino - fotografo" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />{" "}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
