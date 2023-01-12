12fa0f874a046fb0fd48e5b703bdb02cbb086744
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-12 18:21:36 +0100

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html className="">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,12 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Roberto De Martino</title>
         <meta name="description" content="Roberto De Martino - fotografo" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />{" "}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
