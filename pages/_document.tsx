5648281b8bd9ddc90c1ec3c0d0aa4db7a3138702
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-12 18:17:28 +0100

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
